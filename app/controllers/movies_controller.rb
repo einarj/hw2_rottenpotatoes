1d65b0ffe597a89e73a793d75d94e9f6b9426855
Einar Jonsson <ejonsson@handpoint.com>
2012-03-09 13:56:58 +0000

    Now able to sort by title, which becomes highlighted

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,18 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params[:sort_by]
+      if params[:sort_by] == 'title'
+        @title_class = 'hilite'
+      end
+      @movies = Movie.order(params[:sort_by])
+    else
+      @movies = Movie.all
+    end
+  end
+
+  def sort
+    @movies = Movie.order(params[:sort_by])
   end
 
   def new
